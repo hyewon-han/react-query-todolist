ff6d75a7fb693dc6d49fb967baeb30430e335aee
hyewon-han <hw1635@hanmail.net>
2023-12-20 17:42:54 +0900

    Update DetailBox.jsx

--- src/redux/components/DetailBox/DetailBox.jsx
+++ src/redux/components/DetailBox/DetailBox.jsx
@@ -21,20 +21,21 @@ function DetailBox() {
 
   // 이 컴포넌트에서 아이템을 사용하기 위해, params로 전달받은 id를 이용-todo를 filtering
 
-  const { data } = useQuery(QUERY_KEY.TODOS, getTodos, {});
-  const filteredTodos = data.filter((item) => item.id === params.id);
+  const { data: todos } = useQuery(QUERY_KEY.TODOS, getTodos, {});
+  const todo = todos.filter((item) => item.id === params.id);
+  // const filteredTodos = data.filter((item) => item.id === params.id);
 
   // 화면이 최초 렌더링 되는 시점에 올바르지 않은 접근을 차단
   // 지금은 uuidv4()를 사용해서 새로고침할 때 마다 변경 -> DB 또는 Cookie 등 사용하면 해결
-  useEffect(() => {
-    if (filteredTodos.length <= 0 || filteredTodos.length > 1) {
-      alert("올바르지 않은 접근입니다. 메인페이지로 이동합니다.");
-      navigate("/");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (filteredTodos.length <= 0 || filteredTodos.length > 1) {
+  //     alert("올바르지 않은 접근입니다. 메인페이지로 이동합니다.");
+  //     navigate("/");
+  //   }
+  // }, []);
 
   // todo 객체를 얻어옴(filteredTodos는 무조건 요소가 1개여야 함)
-  const todo = filteredTodos[0];
+  // const todo = filteredTodos[0];
 
   // 이전 페이지로 가기 버튼을 선택했을 때, 컴포넌트 이동하는 함수
   const handleButtonClick = () => {
